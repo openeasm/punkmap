a90bbc2a8b1e0e5edd2398bd28d971ce7f28b823
imfht <fiht@qq.com>
2023-07-18 21:23:44 +0800

    add redis probe

--- common/net_tool.go
+++ common/net_tool.go
@@ -1,18 +1,40 @@
 package common
 
-import "net"
+import (
+	"bytes"
+	"fmt"
+	"net"
+)
 
 func ReadAll(conn net.Conn) (data []byte, err error) {
 	// read data from conn
-	buffer := make([]byte, 1024)
+	buffer := make([]byte, 64)
 	var waitTtl = 1
 	for {
 		n, err := conn.Read(buffer)
+		fmt.Println(n)
 		data = append(data, buffer[:n]...)
 		if err != nil && waitTtl == 0 {
 			return data, err
 		}
 		if n < 1024 && waitTtl == 0 {
+			return data, nil
+		}
+		waitTtl--
+	}
+}
+func ReadUntilNewLine(conn net.Conn) (data []byte, err error) {
+	buffer := make([]byte, 1)
+	readCnt := 0
+	waitTtl := 1
+	for {
+		n, err := conn.Read(buffer)
+		data = append(data, buffer[:n]...)
+		readCnt += n
+		if err != nil && waitTtl == 0 {
+			return data, err
+		}
+		if bytes.Contains(buffer[:n], []byte("\n")) {
 			return data, nil
 		}
 		waitTtl--
